f92d761cd1dfd8a08daf8a8f3aacc51c4290920e
Craig R Webster <craig@barkingiguana.com>
2009-03-31 12:18:17 +0100

    Apparently we don't need to pretend we're a controller test.

--- spec/lib/feedback/flash_spec.rb
+++ spec/lib/feedback/flash_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe Feedback::Flash, :type => :controller do
+describe Feedback::Flash do
   before(:each) do
     @helper = Class.new do
       include ActionView::Helpers::TagHelper
