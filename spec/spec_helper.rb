f761d87e73acd8e8e35dfcd2fbdf21e61eef1ee3
Craig R Webster <craig@barkingiguana.com>
2009-03-31 12:50:48 +0100

    Fake out assert_select so we don't need to pull ina huge load of Rails code.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,17 +6,24 @@ require 'hpricot'
 require 'action_view'
 require 'feedback'
 
+
+module FakeAssertSelect
+  def assert_select(tags, selector, content_check = nil)
+    tags.to_s.has_tag?(selector, content_check)
+  end
+end
+
 Spec::Runner.configure do |config|
   config.mock_with :mocha
+  config.include(FakeAssertSelect)
 end
 
 class String
   def has_tag?(selector, content_check = nil)
     elements = (Hpricot(self) / selector).to_a
     if content_check
-      elements.any? { |element| (element / "text()").to_s =~ content_check }
-    else
-      elements.any?
+      elements.select! { |element| (element / "text()").to_s =~ content_check }
     end
+    elements.any? ? elements.to_s : false
   end
 end
