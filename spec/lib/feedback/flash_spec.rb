86067432bf6f787db95dc7252040823bb9277b78
Craig R Webster <craig@barkingiguana.com>
2009-03-31 11:39:51 +0100

    Make sure assert_select is loaded by telling RSpec that this is a controller test.

--- spec/lib/feedback/flash_spec.rb
+++ spec/lib/feedback/flash_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe Feedback::Flash do
+describe Feedback::Flash, :type => :controller do
   before(:each) do
     @helper = Class.new do
       include ActionView::Helpers::TagHelper
